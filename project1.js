1151b629309881c230a49fbfeea0fc3615367740
gouravgupta7799 <118657865+gouravgupta7799@users.noreply.github.com>
2023-01-03 17:31:18 +0530

    Add files via upload

---
+++ project1.js
@@ -0,0 +1,35 @@
+// console.dir(document);
+// console.log(document.domain);
+
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// document.all[10].textContent = 'hello';
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.forms[0]);
+// console.log(document.images);
+
+
+// console.log(document.getElementById('header-title'));
+
+// let headertitle = document.getElementById('header-title');
+// let header = document.getElementById('main-header');
+// console.log(hedertitle);
+// console.log(heardetitle.innertext);
+
+// headertitle.innerHTML = '<h3>hello</h3>';
+
+// headertitle.style.borderBottom = 'solid 3px #000'
+// header.style.borderBottom = 'solid 5px #000'
+
+let headertitle = document.getElementById('header-title');
+headertitle.style.border = 'solid 3px #000'
+let items = document.getElementById('close');
+items.style.color = "green";
+items.style.fontSize = "xxx-large"
