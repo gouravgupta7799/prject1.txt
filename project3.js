32b3ebfb0910f6f9055a09d196d4faf01ed767f6
gouravgupta7799 <118657865+gouravgupta7799@users.noreply.github.com>
2023-01-08 14:02:53 +0530

    Add files via upload

---
+++ project3.js
@@ -0,0 +1,13 @@
+let li = document.getElementsByTagName('li');
+let items = document.getElementsByClassName('list-group-item');
+console.log(li);
+console.log(items);
+
+// items[4].background = 'red'
+// items[4].innerHTML = 'my world'
+
+
+li[4].style.background = 'pink'
+li[4].innerHTML = 'world'
+
+
